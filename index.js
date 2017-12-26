8b359b9678a8cc2033876341716c2f93c05f8477
guysnocussing <13856816+guysnocussing@users.noreply.github.com>
2017-12-25 23:41:50 -0700

    working on moveDodgerRight

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = "I don't do much."
+var dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let rightNumbers = dodger.style.right.replace('px', '');
+    let right = parseInt(rightNumbers, 10);
+
+    if (right <= 0) {
+        dodger.style.right = `${right - 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.which === 37) {
+        moveDodgerLeft();
+    }
+});
+
+document.addEventListener('keydown', function (e) {
+    if (e.which === 39) {
+        moveDodgerRight();
+    }
+});
